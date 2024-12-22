cdb05cfc44c67453fb4a8aebaf609e75a7d1881f
mattmoh <97799575+mattmoh@users.noreply.github.com>
2024-12-22 15:31:46 -0600

    Updated branding

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -71,7 +71,7 @@ return (
     <main className='blog'>
         {data && data.length > 0 ? (
             <>
-                {data[0].post_title && <h1>{toTitleCase(data[0].post_title)}</h1>}
+                {data[0].post_title && <h2>{toTitleCase(data[0].post_title)}</h2>}
                 {data[0].post_body && <p>{data[0].post_body}</p>}
                 {data[0].post_tags && data[0].post_tags.length > 0 && (
                     <ul>
@@ -83,17 +83,17 @@ return (
             </>
         ) : (
             <div>
-                    <p>No blog post found.</p>
-                    <ThreeDots
-                            visible={true}
-                            height="80"
-                            width="80"
-                            color="#808080"
-                            radius="9"
-                            ariaLabel="three-dots-loading"
-                            wrapperStyle={{ textAlign: 'center', display: 'block' }}
-                            wrapperClass=""
-                    />
+                <p>No blog post found.</p>
+                <ThreeDots
+                    visible={true}
+                    height="80"
+                    width="80"
+                    color="#808080"
+                    radius="9"
+                    ariaLabel="three-dots-loading"
+                    wrapperStyle={{ textAlign: 'center', display: 'block' }}
+                    wrapperClass=""
+                />
             </div>
         )}
     </main>
