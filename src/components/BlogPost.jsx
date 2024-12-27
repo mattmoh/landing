1be9f5f18b0045bfe89449c31a9ee4f425154895
mattmoh <97799575+mattmoh@users.noreply.github.com>
2024-12-26 19:19:50 -0600

    updated layout

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,23 +1,15 @@
-// filepath: /c:/Users/mohci/dev/new-mattmoh/src/components/BlogPost.jsx
 import React, { useEffect, useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
 import { ThreeDots } from 'react-loader-spinner';
 import { marked } from 'marked';
-import { HiLink } from 'react-icons/hi'; // Corrected import
+import { format } from 'date-fns';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-const toTitleCase = (str) => {
-  return str.replace(/\w\S*/g, (txt) => {
-    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-  });
-};
-
 const BlogPost = ({ postId }) => {
   const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
@@ -42,67 +34,61 @@ const BlogPost = ({ postId }) => {
     fetchData();
   }, [postId]);
 
-  if (loading) {
-    return (
-      <div>
-        <ThreeDots
-          visible={true}
-          height="80"
-          width="80"
-          color="#808080"
-          radius="9"
-          ariaLabel="three-dots-loading"
-          wrapperStyle={{ textAlign: 'center', display: 'block' }}
-          wrapperClass=""
-        />
-      </div>
-    );
-  }
-
   if (error) {
     return <div>Error: {error}</div>;
   }
 
-return (
-  <main className={data && data.length > 0 && data[0].class ? data[0].class : 'blog'}>
-    {data && data.length > 0 ? (
-      <>
-        {data[0].post_title && (
-          <div dangerouslySetInnerHTML={{ __html: marked(data[0].post_title) }} />
-        )}
-        {data[0].post_body && (
-          <div
-            className="blog-post"
-            dangerouslySetInnerHTML={{
-              __html: marked(data[0].post_body)
-            }}
+  return (
+    <main className={data && data.length > 0 && data[0].class ? data[0].class : 'blog'}>
+      {data && data.length > 0 ? (
+        <>
+          {data[0].post_title && (
+            <div 
+              className={data[0].class + "-title"}
+              dangerouslySetInnerHTML={{
+                __html: marked(data[0].post_title)
+              }}
+            />
+          )}
+          {postId > 1 && data[0].created_at && (
+            <div 
+              className="blog-date">
+              {format(new Date(data[0].created_at), 'MMMM d, yyyy')}
+            </div>
+          )}
+          {data[0].post_body && (
+            <div
+              className="blog-post"
+              dangerouslySetInnerHTML={{
+                __html: marked(data[0].post_body)
+              }}
+            />
+          )}
+          {data[0].post_tags && data[0].post_tags.length > 0 && (
+            <ul className="blog-tags">
+              {data[0].post_tags.map((tag, index) => (
+                <li key={index}>{tag.trim()}</li>
+              ))}
+            </ul>
+          )}
+        </>
+      ) : (
+        <div>
+          <p>No blog post found.</p>
+          <ThreeDots
+            visible={true}
+            height="80"
+            width="80"
+            color="#808080"
+            radius="9"
+            ariaLabel="three-dots-loading"
+            wrapperStyle={{ textAlign: 'center', display: 'block' }}
+            wrapperClass=""
           />
-        )}
-        {data[0].post_tags && data[0].post_tags.length > 0 && (
-          <ul className="blog-tags">
-            {data[0].post_tags.map((tag, index) => (
-              <li key={index}>{tag.trim()}</li>
-            ))}
-          </ul>
-        )}
-      </>
-    ) : (
-      <div>
-        <p>No blog post found.</p>
-        <ThreeDots
-          visible={true}
-          height="80"
-          width="80"
-          color="#808080"
-          radius="9"
-          ariaLabel="three-dots-loading"
-          wrapperStyle={{ textAlign: 'center', display: 'block' }}
-          wrapperClass=""
-        />
-      </div>
-    )}
-  </main>
-);
+        </div>
+      )}
+    </main>
+  );
 };
 
 export default BlogPost;
